305dbea4c21e6d8be826216343728c9c91711d72
schang9 <54689456+schang9@users.noreply.github.com>
2020-01-16 19:59:55 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/* "Hello World", My first program
+Name: Shen Chang
+Date: 2020/01/16
+*/
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+	printf("my major is Mathematics")
+	return 0;
+}
